f05d49f32cd72ffeb71a6a74a40fc6b6894469b1
kevinboco <kevinalexanderboca@gmail.com>
2025-06-13 12:37:53 -0500

    esta si es la conexion

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("mysql.hostinger.com", "u648222299_keboco3", "Bucaramanga3011", "u648222299_zonanorte");
+$conexion = new mysqli("mysql.hostinger.com", "u648222299_keboco3", "Bucaramanga3011", "u648222299_control_acceso");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
