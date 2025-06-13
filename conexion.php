b94daf0e419f15ebad718bc53cd7c4633f4671c7
kevinboco <kevinalexanderboca@gmail.com>
2025-06-13 12:36:57 -0500

    db hostinger

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "control_acceso");
+$conexion = new mysqli("mysql.hostinger.com", "u648222299_keboco3", "Bucaramanga3011", "u648222299_zonanorte");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
